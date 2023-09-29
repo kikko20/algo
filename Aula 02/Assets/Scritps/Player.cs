7c6c530583dd8093c678c193070398fe80a5c935
kikko20 <rychardp58@gmail.com>
2023-09-29 10:57:53 -0300

    esta indo la

--- Aula 02/Assets/Scritps/Player.cs
+++ Aula 02/Assets/Scritps/Player.cs
@@ -6,8 +6,11 @@ public class Player : MonoBehaviour
 {
     private CharacterController controller;
     public float speed;
+    public float gravity;
 
     private Transform cam;
+    
+    Vector3 moveDirection;
     
     public float smoothRotTime;
     private float turnSmoothVelocity;
@@ -22,21 +25,38 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
+        Move();
+    }
 
-        Vector3 direction = new Vector3(horizontal, 0f, vertical);
-
-        if(direction.magnitude > 0)
+    void Move()
+    {
+        if (controller.isGrounded)
         {
-            float angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, angle, ref turnSmoothVelocity, smoothRotTime);
-            
-            transform.rotation = Quaternion.Euler(0f, smoothAngle, 0f);
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
 
-            Vector3 moveDirection = Quaternion.Euler(0f, angle, 0f) * Vector3.forward;
+            Vector3 direction = new Vector3(vertical, 0f, horizontal);
+
+            if(direction.magnitude > 0)
+            {
+                float angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+                float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, angle, ref turnSmoothVelocity, smoothRotTime);
             
-            controller.Move(moveDirection * speed * Time.deltaTime);
+                transform.rotation = Quaternion.Euler(0f, smoothAngle, 0f);
+
+                moveDirection = Quaternion.Euler(0f, angle, 0f) * Vector3.forward * speed;
+            
+                
+            }
+            else
+            {
+                moveDirection = Vector3.zero;
+            }
         }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+        
+        controller.Move(moveDirection * Time.deltaTime);
+
     }
 }
